1e1ff72c4c196c7a7703ac4495f16db8509adda4
Rabia Sultana <97949638+RabiaSultana@users.noreply.github.com>
2022-01-18 17:28:13 +0600

    Create Rabia_stringlec.php
    HW-1 stringlec.php

---
+++ Rabia_stringlec.php
@@ -0,0 +1,12 @@
+<?php
+$fname = "Rabia";
+$lname = "Sultana";
+$name = "Rabia Sultana";
+echo $fname," ",$lname . "</br>";
+echo $fname. " " . $lname. "</br>";
+echo strrev($name). "</br>";
+echo strlen($name)."</br>";
+
+echo "class-1 HW- Batch-2" .  "</br>";
+
+?>
