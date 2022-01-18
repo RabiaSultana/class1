5a9ee87f38bdabe046cd267f42710e1c0e9e6fd6
Rabia Sultana <97949638+RabiaSultana@users.noreply.github.com>
2022-01-18 19:06:12 +0600

    Create Rabia_operator.php
    HW-1, Batch 2, Rabia Sultana.

---
+++ Rabia_operator.php
@@ -0,0 +1,49 @@
+<?php 
+
+
+$A = 8;
+
+$B = 9;
+
+echo "A = "."$A</br>";
+
+echo "B = "."$B</br>";
+
+echo "</br>";
+
+echo "1. Addition"."</br>";
+echo "A + B"." = "."$A"." + "."$B"."</br>"."= ";
+echo $A + $B ."</br>";
+
+echo "</br>";
+
+echo "2. Subtraction"."</br>";
+echo "A - B"." = "."$A"." - "."$B"."</br>"."= ";
+echo $A - $B."</br>";
+
+echo "</br>";
+
+echo "3. Multiplication"."</br>";
+echo "A * B"." = "."$A"." * "."$B"."</br>"."= ";
+echo $A * $B."</br>";
+
+echo "</br>";
+
+echo "4. Division"."</br>";
+echo "A / B"." = "."$A"." / "."$B"."</br>"."= ";
+echo $A /$B."</br>";
+
+echo "</br>";
+
+echo "5. Modulus"."</br>";
+echo "A % B"." = "."$A"." % "."$B"."</br>"."= ";
+echo $A % $B."</br>";
+
+echo "</br>";
+
+echo "6. Exponentiation"."</br>";
+echo "A ** B"." = "."$A"." ** "."$B"."</br>"."= ";
+echo $A ** $B."</br>";
+
+
+?>
